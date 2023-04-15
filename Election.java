7c54fdeaf226deca0940cf712dce024f6504a72c
ChaosShadoww <113145351+ChaosShadoww@users.noreply.github.com>
2023-04-14 23:11:55 -0400

    Add files via upload

---
+++ Election.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+public class Election {
+
+    
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        String sCanName = " ";
+        double dVotes = 0.0;
+        
+        
+        
+        System.out.println("Enter the Canadite name: ");
+        sCanName = input.next();
+        
+        System.out.println("Enter the Number of Votes: ");
+        dVotes = input.nextDouble();
+        
+        
+
+
+
+
+
+    }
+    
+}
